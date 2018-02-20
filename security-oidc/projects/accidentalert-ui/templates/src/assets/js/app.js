4fda6de6638caf33ac0442a9c0b555d7b4951006
Hugo Guerrero <hguerrer@redhat.com>
2018-02-20 16:19:41 -0600

    sending form as json object for new version of report service

--- security-oidc/projects/accidentalert-ui/templates/src/assets/js/app.js
+++ security-oidc/projects/accidentalert-ui/templates/src/assets/js/app.js
@@ -1,6 +1,21 @@
 (function($, document, window){
 	
 	$(document).ready(function(){
+		$.fn.serializeObject = function() {
+			var o = {};
+			var a = this.serializeArray();
+			$.each(a, function() {
+				if (o[this.name]) {
+					if (!o[this.name].push) {
+						o[this.name] = [o[this.name]];
+					}
+					o[this.name].push(this.value || '');
+				} else {
+					o[this.name] = this.value || '';
+				}
+			});
+			return o;
+		};
 
 		// Cloning main navigation for mobile menu
 		$(".mobile-navigation").append($(".main-navigation .menu").clone());
